7fc60e271f8f7c8525c885ce33c84bb4fcd11747
Gord00 <46492333+Gord00@users.noreply.github.com>
2019-02-07 11:16:42 -0500

    Add files via upload

---
+++ Turtletime/scripts.js
@@ -0,0 +1,20 @@
+// Interactivity demo 2
+function revealmessage(){
+	document.getElementById ("hiddenMessage").style.display=  'block'
+}
+
+
+function countDown(){
+	var currentVal= document.getElementById("countDownButton").innerHTML;
+	var newVal=0;
+	if (currentVal>0){
+	   newVal=currentVal- 1;
+}
+	document.getElementById("countDownButton").innerHTML=newVal;
+}
+
+
+
+
+
+//
